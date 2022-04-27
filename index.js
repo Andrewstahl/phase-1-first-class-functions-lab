682bea4d21ca0d72a8360aa66fbbb6dbedfd4c07
Andrew Stahl <andrewstahl96@gmail.com>
2022-04-26 21:22:26 -0400

    commit tree dca1ed8764052356aff916645bca83c88a027a22

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+// Returns the first two drivers in an array
+const returnFirstTwoDrivers = function (arr) {return arr.slice(0, 2)};
+// Returns the last two drivers in an array
+const returnLastTwoDrivers = function (arr) {return arr.slice(-2)};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+  return function (fare) {return fare * num};
+}
+
+const fareDoubler = function (fare) {return createFareMultiplier(2)(fare)};
+const fareTripler = function (fare) {return createFareMultiplier(3)(fare)};
+
+function selectDifferentDrivers(arrDrivers, secondaryFunction) {
+  if (secondaryFunction === returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrDrivers);
+  } else if (secondaryFunction === returnLastTwoDrivers) {
+    return returnLastTwoDrivers(arrDrivers);
+  }
+}
+
+// console.log(returnFirstTwoDrivers ([1, 2, 3, 4]));
